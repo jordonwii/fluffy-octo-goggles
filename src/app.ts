b8badb591e1c6c6158370b9eab3bb01d1d52a3a3
Jordon Wing <jordonwii@gmail.com>
2019-01-27 20:24:03 -0800

    Switch to using Node for the server.
    This adds browserify in order to allow for the same import syntax on
    client and server. To run in debug mode, use `npm run watch-debug`.
    
    Thanks to https://github.com/Microsoft/TypeScript-Node-Starter for
    inspiration on the layout, etc.

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+
+// Controllers (route handlers)
+import * as homeController from "./controllers/home";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+app.use(bodyParser.json());
+
+app.use(
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController.index);
+
+export default app;
