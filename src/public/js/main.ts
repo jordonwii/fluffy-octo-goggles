3a90258fb179cc95ac590126713f08e1fb726324
Jordon Wing <jordonwii@gmail.com>
2019-02-10 16:34:14 -0800

    Remove dead game var.

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -2,7 +2,6 @@ import {Game} from "./game";
 
 // Gives us access to the game for debugging.
 // TODO (probably?) delete this for anything real.
-var game;
 window.addEventListener("DOMContentLoaded", function() {
   let game: Game = new Game();
   game.buildMaze();
